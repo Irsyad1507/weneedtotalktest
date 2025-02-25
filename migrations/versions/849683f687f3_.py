150e48869fcec00353d0f455f47bc1a2da882dcd
Irsyad1507 <amirulirsyad157@gmail.com>
2025-02-25 18:15:56 +0800

    Added Table

---
+++ migrations/versions/849683f687f3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 849683f687f3
+Revises: 
+Create Date: 2025-02-25 18:15:03.857236
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '849683f687f3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
